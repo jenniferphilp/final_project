207bd8431a3361c127a88eb263834ef26c7be480
jenniferphilp <jennifer.philp@gmail.com>
2016-12-01 17:39:50 -0500

    commit

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 
 
- app.use(express.static(__dirname + 'build'));
+ app.use(express.static(__dirname + '/build'));
 
 // app.use(express.static('public'));
 
